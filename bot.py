5bfb8001359455536c062155496f1c8d1aaa1966
nom <93007593+noemtdev@users.noreply.github.com>
2024-01-06 17:18:54 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,22 +14,24 @@ async def on_ready():
         description = "Kicks all members that don't have a role"
 )
 async def kickall(ctx):
-    if ctx.author.guild_permissions.kick_members:
-        members = ctx.guild.members
-        for member in members:
-            if len(member.roles) == 1:
-                try:
-                    await member.kick(reason = 'Kicking all members without roles')
-                    await ctx.send(f'Kicked {member.display_name}')
-                except discord.Forbidden:
-                    await ctx.send('I do not have kick permissions')
-                    pass
-                except discord.HTTPException as e:
-                    await ctx.send(f'Failed to kick {member.display_name}')
-            else:
-                pass        
-    else:
-        await ctx.send('You do not have the required permissions')    
+    if not ctx.author.guild_permissions.kick_members:
+        return await ctx.send('You do not have the required permissions')    
+        
+    for member in ctx.guild.members:
+        if member.roles:
+            try:
+                await member.kick(reason = 'Kicking all members without roles')
+                await ctx.send(f'Kicked {member.display_name}')
+                
+            except discord.Forbidden:
+                await ctx.send(f'Failed to kick {member.display_name} due to missing permissions.')
+                continue
+                
+            except discord.HTTPException as e:
+                await ctx.send(f'Failed to kick {member.display_name}')
+        else:
+            pass        
+
 
 
 bot.run('TOKEN')
